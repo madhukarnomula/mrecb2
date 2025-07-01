0a2d8c25b6b1e2e06826ad4889b635fbcf459457
madhukarnomula <madhunomula14@gmail.com>
2025-07-01 10:54:17 +0530

    second_max.py
    Changed to find second max in list

--- max_in_list.py
+++ max_in_list.py
@@ -2,7 +2,9 @@ a=[2,4,2,7,9,10,3,4]
 max=a[0]
 smax=max
 for i in range(len(a)):
-    if a[i]>max:
+    if (a[i]>max):
         smax=max
         max=a[i]
+    elif(a[i]>smax and a[i]<max):
+        smax=a[i]
 print(smax)
