6c1703eaaa82129775f988a15eb2a640d4a79b6e
madhukarnomula <madhunomula14@gmail.com>
2025-07-26 23:36:48 +0530

    Add files via upload

---
+++ trycatch.js
@@ -0,0 +1,13 @@
+let a=10;
+let b=15;
+console.log(a);
+console.log(b);
+console.log("a+b=",a+b);
+try{
+    console.log("a+b=",a+c);
+}catch(err){
+    console.log(err);
+}
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
